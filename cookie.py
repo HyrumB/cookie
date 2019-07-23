bd063a2f42315266d7966639778013e1051da9b2
Calvin Bullock <calvinnbullock@gmail.com>
2019-07-23 16:41:24 -0700

    print_1 added

--- cookie.py
+++ cookie.py
@@ -5,14 +5,22 @@ variables = {}
 def getValue(v1):
     if v1 in variables:
         return variables[v1]
-    
+
     return int(v1)
+
+def print_1():
+    if "_1" in variables:
+        print(variables["_1"])
 
 def processLine(line):
     tokens = line.split(" ")
     # Case: var = v1
     if len(tokens) == 3 and tokens[1] == "=":
         variables[tokens[0]] = getValue(tokens[2])
+        return
+
+    if len(tokens) == 1:
+        print_1()
         return
 
 for line in sys.stdin:
