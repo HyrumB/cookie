4e975f341ee8b053fef7c77c05419de0c5ddea72
Hyrum <hyrumbullock@gmail.com>
2019-07-23 16:28:13 -0700

    implemented add() function

--- cookie.py
+++ cookie.py
@@ -10,11 +10,19 @@ def getValue(v1):
 
 def processLine(line):
     tokens = line.split(" ")
+
+    # Case: var = add()
+    if len(tokens) == 3 and tokens[2] == "add()":
+        variables[tokens[0]] = getValue("_1") + getValue("_2")
+        return
+
+
     # Case: var = v1
     if len(tokens) == 3 and tokens[1] == "=":
         variables[tokens[0]] = getValue(tokens[2])
         return
 
+   
 for line in sys.stdin:
     line = line.strip()
     if line == "stop":
